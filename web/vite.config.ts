128270ff80c038c239f2efc111903262b540f69b
양지성 <js.yang@wolilab.com>
2026-07-17 19:01:59 +0900

    홍보 페이지 추가

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,17 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        privacy: resolve(__dirname, "privacy-policy.html"),
+        support: resolve(__dirname, "support.html"),
+        web: resolve(__dirname, "web.html"),
+      },
+    },
+  },
 });
